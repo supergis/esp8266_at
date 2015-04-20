c6bc8cf4d3035ae346cd358e94bb2cdfe87d2ecc
supergis <wangeq33@gmail.com>
2015-04-20 11:50:04 +0800

    update ...

--- at/user/at_wifiCmd.c
+++ at/user/at_wifiCmd.c
@@ -15,6 +15,8 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <string.h>
+
 #include "user_interface.h"
 #include "at.h"
 #include "at_wifiCmd.h"
